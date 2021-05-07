75eef58d5b23f8bac17be1abeffd5648af63dc97
Julia Ayres <j.bobrineva@gmail.com>
2021-05-07 16:54:11 -0400

    added API with events and user functions to link them to backend

---
+++ src/Api.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class. Static class tying together methods used to get/send to the API. */
+
+class BoogieBotApi {
+	// the token to interact with the API is stored here.
+	static token;
+
+	static async request (endpoint, data = {}, method = "get") {
+		const url = `${BASE_URL}/${endpoint}`;
+		const headers = { Authorization: `Bearer ${BoogieBotApi.token}` };
+		const params = method === "get" ? data : {};
+
+		try {
+			return (await axios({ url, method, data, params, headers })).data;
+		} catch (err) {
+			console.log("err", err);
+			let message = err.response.data.error.message;
+
+			throw Array.isArray(message) ? message : [ message ];
+		}
+	}
+
+	/** Get events filtered by title, and single event filteres by id. */
+
+	static async getEvents (title) {
+		let res = await this.request("events", { title });
+		return res.events;
+	}
+
+	static async getEvent (id) {
+		let res = await this.request(`events/${id}`);
+		return res.event;
+	}
+
+	/** Get current user */
+
+	static async getCurrentUser (username) {
+		let res = await this.request(`users/${username}`);
+		return res.user;
+	}
+
+	/** Get token for login from username, password. */
+
+	static async login (data) {
+		let res = await this.request(`auth/token`, data, "post");
+		return res.token;
+	}
+
+	/** Signup for site. */
+
+	static async signup (data) {
+		let res = await this.request(`auth/register`, data, "post");
+		return res.token;
+	}
+
+	/** Save user profile page. */
+
+	static async saveProfile (username, data) {
+		let res = await this.request(`users/${username}`, data, "patch");
+		return res.user;
+	}
+
+	// /** Claim Event as Host */
+
+	// static async claimEventAsHost (username, id) {
+	// 	await this.request(`users/${username}/events/${id}`, {}, "post");
+	// }
+}
+export default BoogieBotApi;
